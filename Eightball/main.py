3b477a21b694fa1ee826d25659b0205d62338f09
Emphisys <70330833+Emphisys@users.noreply.github.com>
2021-05-20 16:03:20 +0100

    Updated code that reflects exe file created

--- Eightball/main.py
+++ Eightball/main.py
@@ -2,11 +2,28 @@ from Name import *
 from Q import enter_question
 from Answer import numbergen
 
+
+
 def program():
-  print(enter_name())
-  print(enter_question())
-  print(numbergen())
+  print(enter_name(), "\n")
+  print(enter_question(), "\n")
+  print("Magic 8-Ball's Answer:", numbergen(), "\n")
   #print(enter_name, " asks: ", enter_question, "\n", "Magic 8-Ball's Answer: ", numbergen)
+
+def restart():
+  res =input("Would you like to try again? If so, press 'r'")
+  print("\n")
+  if res == "r":
+    program()
+    restart()
+  elif res == "R":
+    program()
+    restart()
+  else:
+    res == "closes"
 
 
 program()
+restart()
+
+
